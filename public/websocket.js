e5a97c9c42c24b5acd9b623eb2f9703669387fb3
MPS <99948930+thediscorddev@users.noreply.github.com>
2024-11-20 15:52:11 +0700

    Update websocket.js

--- public/websocket.js
+++ public/websocket.js
@@ -1,7 +1,7 @@
 const protocol = window.location.protocol === 'https:' ? 'wss:' : 'ws:';
 const host = window.location.hostname;
 const port = window.location.port || 3000; // Use 3000 if no port is specified
-const wsUrl = protocol + "//" + host + ":" + port;
+const wsUrl = protocol + "//" + host + ":";
 const socket = new WebSocket(wsUrl);
 socket.onopen = function(event) {
     console.log('Connection opened:', event);
